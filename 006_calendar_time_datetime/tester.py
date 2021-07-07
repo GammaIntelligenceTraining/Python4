1a8f347404c564520806b8bf9a162d41cbb9925d
RomanGammaIntelligence <roman@gamma-intelligence.com>
2021-07-07 16:51:01 +0300

    Datetime

---
+++ 006_calendar_time_datetime/tester.py
@@ -0,0 +1,13 @@
+import datetime
+
+dt_str = 'November 30, 2020, 15:45'
+dt_str1 = '12:28:33 Nov 30, 2020'
+dt_str2 = 'Wed, 07/07/21'
+#
+# dt_str3 = '7 Июнь 2021, 12:56'
+# dt_str3 = dt_str3.replace('Июнь', 'June')
+# str_to_date3 = datetime.datetime.strptime(dt_str3, '%d %B %Y, %H:%M')
+# print(str_to_date3)
+# print(type(str_to_date3))
+# today = datetime.datetime.today()
+# print(today - str_to_date3)
